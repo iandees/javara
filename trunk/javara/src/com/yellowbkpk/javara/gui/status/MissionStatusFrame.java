e1b20ff0e6c9b473b778d6699d7313c37cff3b2e
ian.dees <ian.dees@99d860a3-0a1e-0410-9d02-1ff476707afe>
2007-05-10 03:55:31 +0000

    Menu bar pieces added to the mission status frame.

--- trunk/javara/src/com/yellowbkpk/javara/gui/status/MissionStatusFrame.java
+++ trunk/javara/src/com/yellowbkpk/javara/gui/status/MissionStatusFrame.java
@@ -1,7 +1,12 @@
 package com.yellowbkpk.javara.gui.status;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
 
 public class MissionStatusFrame extends JFrame {
-
+    public MissionStatusFrame() {
+        // Add menu bar
+        JMenuBar menuBar = new MissionStatusFrameMenuBar();
+        setJMenuBar(menuBar);
+    }
 }
