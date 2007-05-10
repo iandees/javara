6cfc8016a79cce8ea71cedc4d457d20551afe65a
ian.dees <ian.dees@99d860a3-0a1e-0410-9d02-1ff476707afe>
2007-05-10 03:13:43 +0000

    Status bar is undecorated and has a border.

--- trunk/javara/src/com/yellowbkpk/javara/gui/status/StatusBarFrame.java
+++ trunk/javara/src/com/yellowbkpk/javara/gui/status/StatusBarFrame.java
@@ -1,7 +1,24 @@
 package com.yellowbkpk.javara.gui.status;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class StatusBarFrame extends JFrame {
+    
+    private JPanel contentOfUndecoratedFrame;
 
+    public StatusBarFrame() {
+        super();
+        setPreferredSize(new Dimension(640, 100));
+        setLocation(0, 480);
+        setUndecorated(true);
+        
+        contentOfUndecoratedFrame = new JPanel();
+        contentOfUndecoratedFrame.setBorder(BorderFactory.createLineBorder(Color.black));
+        add(contentOfUndecoratedFrame);
+    }
 }
