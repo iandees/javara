d392cc565bc86a53c80ec0016a279a6768834974
ian.dees <ian.dees@99d860a3-0a1e-0410-9d02-1ff476707afe>
2007-05-10 03:42:08 +0000

    Can drag the frame without a title bar.

--- trunk/javara/src/com/yellowbkpk/javara/gui/status/StatusBarFrame.java
+++ trunk/javara/src/com/yellowbkpk/javara/gui/status/StatusBarFrame.java
@@ -2,6 +2,10 @@ package com.yellowbkpk.javara.gui.status;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
@@ -10,6 +14,9 @@ import javax.swing.JPanel;
 public class StatusBarFrame extends JFrame {
     
     private JPanel contentOfUndecoratedFrame;
+    protected Point dragStart;
+    protected Point location;
+    protected MouseEvent pressed;
 
     public StatusBarFrame() {
         super();
@@ -19,6 +26,20 @@ public class StatusBarFrame extends JFrame {
         
         contentOfUndecoratedFrame = new JPanel();
         contentOfUndecoratedFrame.setBorder(BorderFactory.createLineBorder(Color.black));
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent arg0) {
+                pressed = arg0;
+            }
+        });
+        addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent arg0) {
+                location = getLocation(location);
+                int x = location.x - pressed.getX() + arg0.getX();
+                int y = location.y - pressed.getY() + arg0.getY();
+                setLocation(x, y);
+            }
+        });
         add(contentOfUndecoratedFrame);
     }
+
 }
