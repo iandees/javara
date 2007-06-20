559ce9a4992fb5a8c17ba358fb9856e5752c2ec1
ian.dees <ian.dees@99d860a3-0a1e-0410-9d02-1ff476707afe>
2007-06-20 05:59:52 +0000

    Time for bed...

--- trunk/javara/src/com/yellowbkpk/jnova/client/game/Vector2D.java
+++ trunk/javara/src/com/yellowbkpk/jnova/client/game/Vector2D.java
@@ -2,6 +2,7 @@ package com.yellowbkpk.jnova.client.game;
 
 public class Vector2D {
 
+    private static final Vector2D ORIGIN = new Vector2D(0f, 0f);
     public float x;
     public float y;
 
@@ -24,6 +25,35 @@ public class Vector2D {
 
     public Vector2D multiply(float xComp, float yComp) {
         return new Vector2D(x * xComp, y * yComp);
+    }
+
+    public void rotateAround(Vector2D origin, float deg) {
+        Vector2D shifted = new Vector2D(this.x - origin.x, this.y - origin.y);
+        x = (float) (Math.cos(deg) * shifted.x - Math.sin(deg) * shifted.y);
+        y = (float) (Math.cos(deg) * shifted.y + Math.sin(deg) * shifted.x);
+    }
+
+    public void scale(float scaleValue) {
+        x *= scaleValue;
+        y *= scaleValue;
+    }
+    
+    public float length() {
+        return (float) Math.sqrt((x*x)+(y*y));
+    }
+
+    public void normalize() {
+        float l = length();
+        x = x / l;
+        y = y / l;
+    }
+
+    public Vector2D multiply(Vector2D other) {
+        return this.multiply(other.x, other.y);
+    }
+
+    public Vector2D add(Vector2D other) {
+        return this.add(other.x, other.y);
     }
 
 }
