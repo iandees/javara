d7b60c56a34cb3154cb40ea4e6a2d232aa973cc0
ian.dees <ian.dees@99d860a3-0a1e-0410-9d02-1ff476707afe>
2007-07-03 18:49:00 +0000

    Trying to use a better memory -> file -> URL caching system.

--- javara/src/com/yellowbkpk/maps/gui/ImageCache.java
+++ javara/src/com/yellowbkpk/maps/gui/ImageCache.java
@@ -5,17 +5,25 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.MediaTracker;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageOutputStream;
 import javax.swing.ImageIcon;
 
 public class ImageCache {
     
     private static final int TILE_SIZE = 256;
+    private static final String CACHE_DIR = "cache";
+    private static final int MAX_CACHE_SIZE = 128;
     ConcurrentHashMap<String, Image> cacheMap;
     Map<String, Thread> imgFetchers;
     private String baseURL = "http://mt3.google.com/mt?n=404&v=w2.56&";
@@ -43,7 +51,7 @@ public class ImageCache {
         if (y > tilesAtZoom) {
             return noImageTile;
         }
-        
+
         int correctedX = x;
         if (x >= tilesAtZoom) {
             correctedX = x - tilesAtZoom;
@@ -52,48 +60,93 @@ public class ImageCache {
         if (x < 0) {
             correctedX = x + tilesAtZoom;
         }
-        
+
         final String url = baseURL + "x=" + correctedX + "&y=" + y + "&zoom=" + zoom;
 
         Image image = cacheMap.get(url);
-
+        // If the image was not in memory cache
         if (image == null) {
-            Thread thread = imgFetchers.get(url);
-            if (thread == null) {
-                Thread imgFetcher = new Thread(new Runnable() {
-                    public void run() {
-                        URL u = null;
-                        try {
-                            u = new URL(url);
-                        } catch (MalformedURLException e) {
-                            e.printStackTrace();
-                            System.err.println("Bad URL");
-                            return;
+            // If the image was not on disk cache
+            final String filename = CACHE_DIR + "/" + zoom + "/" + x + "-" + y + ".png";
+            final File file = new File(filename);
+            
+            if (file.exists()) {
+                System.err.print("Cache: File exists: ");
+                Thread thread = imgFetchers.get(url);
+                if (thread == null) {
+                    System.err.println("starting file fetcher");
+                    Thread imgFetcher = new Thread(new Runnable() {
+                        public void run() {
+                            Image image = new ImageIcon(file.getPath()).getImage();
+                            cacheMap.put(url, image);
+                            imgFetchers.remove(url);
                         }
-
-                        ImageIcon i = new ImageIcon(u);
-
-                        if (i.getImageLoadStatus() == MediaTracker.COMPLETE) {
-                            cacheMap.put(url, i.getImage());
-                        } else {
-                            System.err.println("Could not load " + u + " code was " + i.getImageLoadStatus());
-                        }
-                    }
-                });
-                imgFetchers.put(url, imgFetcher);
-                imgFetcher.start();
-                return null;
+                    });
+                    imgFetchers.put(url, imgFetcher);
+                    imgFetcher.start();
+                    return null;
+                } else {
+                    System.err.println("file already being fetched");
+                    // Already fetching it from a file.
+                    return null;
+                }
             } else {
-                // Already fetching it.
-                return null;
+                System.err.print("Cache: File does not exist: ");
+                // If the image is not already being fetched
+                Thread thread = imgFetchers.get(url);
+                if (thread == null) {
+                    System.err.println("starting url fetcher");
+                    Thread imgFetcher = new Thread(new Runnable() {
+                        public void run() {
+                            URL u = null;
+                            try {
+                                u = new URL(url);
+                            } catch (MalformedURLException e) {
+                                e.printStackTrace();
+                                System.err.println("Bad URL");
+                                return;
+                            }
+
+                            ImageIcon i = new ImageIcon(u);
+
+                            if (i.getImageLoadStatus() == MediaTracker.COMPLETE) {
+                                cacheMap.put(url, i.getImage());
+                                
+                                // This chunk here saves the image icon to a file
+                                BufferedImage bI = new BufferedImage(i.getIconWidth(), i.getIconWidth(), BufferedImage.TYPE_INT_ARGB);
+                                Graphics bIGraphics = bI.getGraphics();
+                                bIGraphics.drawImage(i.getImage(), 0, 0, null);
+                                bIGraphics.dispose();
+                                try {
+                                    file.mkdirs();
+                                    ImageIO.write(bI, "PNG", file);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            } else {
+                                System.err.println("Could not load " + u + " code was " + i.getImageLoadStatus());
+                            }
+
+                            imgFetchers.remove(url);
+                        }
+                    });
+                    imgFetchers.put(url, imgFetcher);
+                    imgFetcher.start();
+                    return null;
+                } else {
+                    System.err.println("already fetching url");
+                    // Already fetching it.
+                    return null;
+                }
             }
         } else {
+            if(cacheMap.size() > MAX_CACHE_SIZE) {
+                System.err.println("Cache: Clearing the cache map");
+                cacheMap.clear();
+            }
+            
             return image;
         }
-    }
-
-    private void correctRotatedEarth(int x, int y, int zoom) {
-        
     }
 
 }
