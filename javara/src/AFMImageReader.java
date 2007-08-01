d5cf2d38552dcf91cbfaaeaafee2037f807089db
ian.dees <ian.dees@99d860a3-0a1e-0410-9d02-1ff476707afe>
2007-08-01 20:38:55 +0000

    Double-click to mask and count above a certain height.

--- javara/src/AFMImageReader.java
+++ javara/src/AFMImageReader.java
@@ -1,4 +1,7 @@
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,13 +19,15 @@ public class AFMImageReader {
     private static final String PREFIX = "D:\\Documents and Settings\\212034767\\Desktop\\";
     private static final int FIELD_X = 512;
     private static final int FIELD_Y = 512;
-    private static int maxResult = Integer.MIN_VALUE;
-    private static int minResult = Integer.MAX_VALUE;
+    private static final double HARD_SCALE = 0.006713765; // V/LSB
+    private static final double SOFT_SCALE = 13.68092; // nm/V
+    private static double maxResult = Integer.MIN_VALUE;
+    private static double minResult = Integer.MAX_VALUE;
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         File f = new File(PREFIX + "100NCLAG.008");
-        int[][] dataArr = new int[FIELD_Y][FIELD_X];
+        final double[][] dataArr = new double[FIELD_Y][FIELD_X];
         
         try {
             FileInputStream is = new FileInputStream(f);
@@ -32,8 +37,11 @@ public class AFMImageReader {
                 for(int y = 0; y < FIELD_Y; y++) {
                     is.read(sArr);
                     
-                    // Convert to short
-                    int result = sArr[0] + (sArr[1] << 8);
+                    // Convert to unsigned short
+                    int lsb = sArr[0] + (sArr[1] << 8);
+                    
+                    // Convert to actual height
+                    double result = (double) lsb * HARD_SCALE * SOFT_SCALE;
                     
                     if(result < minResult) {
                         minResult = result;
@@ -53,7 +61,7 @@ public class AFMImageReader {
             e.printStackTrace();
         }
         
-        BufferedImage img = new BufferedImage(FIELD_X, FIELD_Y, BufferedImage.TYPE_INT_RGB);
+        final BufferedImage img = new BufferedImage(FIELD_X, FIELD_Y, BufferedImage.TYPE_INT_RGB);
         
         for(int x = 0; x < FIELD_X; x++) {
             for(int y = 0; y < FIELD_Y; y++) {
@@ -70,7 +78,34 @@ public class AFMImageReader {
         
         JFrame frame = new JFrame("Image!");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(new JLabel(new ImageIcon(img)));
+        final JLabel label = new JLabel(new ImageIcon(img));
+        label.addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseMoved(MouseEvent e) {
+                System.out.println(e.getX()+","+e.getY()+" = "+dataArr[e.getY()][e.getX()]);
+            }
+        });
+        label.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                if (e.getClickCount() == 2) {
+                    double heightAtClick = dataArr[e.getY()][e.getX()];
+
+                    int count = 0;
+                    int rgb = Color.black.getRGB();
+                    for (int x = 0; x < FIELD_X; x++) {
+                        for (int y = 0; y < FIELD_Y; y++) {
+                            if (dataArr[y][x] > heightAtClick) {
+                                count++;
+                            } else {
+                                img.setRGB(x, y, rgb);
+                            }
+                        }
+                    }
+                    label.repaint();
+                    System.out.println("Above " + heightAtClick + " = " + count);
+                }
+            }
+        });
+        frame.add(label);
         frame.pack();
         frame.setVisible(true);
     }
