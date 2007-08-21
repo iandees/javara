b37b13367abcf1535c3913a4d11caf7f4a9aeec4
ian.dees <ian.dees@99d860a3-0a1e-0410-9d02-1ff476707afe>
2007-08-21 21:32:27 +0000

    Adding a file-based mysql outputter.

---
+++ trunk/javara/src/com/yellowbkpk/termscrape/FileMySQLScraperOutput.java
@@ -0,0 +1,54 @@
+package com.yellowbkpk.termscrape;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileMySQLScraperOutput implements ScraperOutput {
+    private BufferedWriter output;
+
+    public FileMySQLScraperOutput(String filename) {
+        try {
+            File f = new File(filename);
+            output = new BufferedWriter(new FileWriter(f));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addSection(Section theSection) {
+        String line = "INSERT INTO `classes` " +
+        "(department, block, section, sectionURL, credits, division, professor, title, freshmanOnly, prerequisite, year, enrollment, capacity, building, roomnumber) "+
+        "VALUES " +
+        "('"+theSection.getDepartment()+"'," +
+        " '"+theSection.getBlock()+"'," +
+        " '"+theSection.getSectionID()+"'," +
+        " '"+theSection.getCatalogURL()+"'," +
+        " '"+theSection.getCredits()+"'," +
+        " '"+theSection.getDivision()+"'," +
+        " '"+theSection.getFaculty()+"'," +
+        " '"+theSection.getTitle()+"'," +
+        " '"+(theSection.getFreshmanOnly()?1:0)+"'," +
+        " '"+theSection.getPrerequisites()+"'," +
+        " '"+theSection.getYear()+"'," +
+        " '"+theSection.getEnrollment()+"'," +
+        " '"+theSection.getCapacity()+"'," +
+        " '"+theSection.getBuilding()+"'," +
+        " '"+theSection.getRoomnumber()+"')";
+        try {
+            output.write(line + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void cleanup() {
+        try {
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
