da395573214136513a87dab0d8e408364e5c8302
ian.dees <ian.dees@99d860a3-0a1e-0410-9d02-1ff476707afe>
2007-08-21 18:41:24 +0000

    Adding a radar version of things.

--- trunk/javara/src/com/yellowbkpk/maps/GGroundOverlay.java
+++ trunk/javara/src/com/yellowbkpk/maps/GGroundOverlay.java
@@ -19,28 +19,31 @@ public class GGroundOverlay implements GOverlay {
 
     private ImageIcon image;
     private GLatLngBounds myBounds;
+    private URL myURL;
 
     /**
      * @param imageURL
      * @param bounds
      */
     public GGroundOverlay(String imageURL, GLatLngBounds bounds) {
-        URL u = null;
+        myURL = null;
         try {
-            System.out.println(imageURL);
-            u = new URL(imageURL);
-            System.out.println(u);
+            myURL = new URL(imageURL);
         } catch (MalformedURLException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        image = new ImageIcon(u);
-        System.out.println(image.getImageLoadStatus());
         myBounds = bounds;
     }
 
     public void drawOverlay(Graphics2D dbg2, Point nwPoint, int zoom, GLatLngBounds viewBounds,
             ImageObserver imageObserver) {
+        if (image == null) {
+            System.out.println("Gotta load " + myURL + ".");
+            image = new ImageIcon(myURL);
+            System.out.println(image.getImageLoadStatus());
+        }
+        
         int dx1 = GoogleMapUtilities.lngToX(myBounds.getNorthwest().getLongitude(), 17 - zoom) - nwPoint.x;
         int dy1 = GoogleMapUtilities.latToY(myBounds.getNorthwest().getLatitude(), 17 - zoom) - nwPoint.y;
         int dx2 = GoogleMapUtilities.lngToX(myBounds.getSoutheast().getLongitude(), 17 - zoom) - nwPoint.x;
@@ -54,7 +57,6 @@ public class GGroundOverlay implements GOverlay {
         if (image.getImageLoadStatus() == MediaTracker.COMPLETE) {
             dbg2.drawImage(image.getImage(), dx1, dy1, dx2, dy2, sx1, sy1, sx2, sy2, imageObserver);
         } else {
-            System.out.println(image.getImageLoadStatus());
         }
     }
 
