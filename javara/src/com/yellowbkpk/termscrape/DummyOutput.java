b880e496b5357a2e222621958227e596aa841ecf
ian.dees <ian.dees@99d860a3-0a1e-0410-9d02-1ff476707afe>
2007-08-21 21:20:47 +0000

    Adding a dummy outputter for testing and a scraper for the prerequisites page.

---
+++ javara/src/com/yellowbkpk/termscrape/DummyOutput.java
@@ -0,0 +1,13 @@
+package com.yellowbkpk.termscrape;
+
+public class DummyOutput implements ScraperOutput {
+
+    public void addSection(Section theSection) {
+
+    }
+
+    public void cleanup() {
+
+    }
+
+}
