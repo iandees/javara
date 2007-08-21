cd07598d6774cbcb5b3e3773cc307f865eda5388
ian.dees <ian.dees@99d860a3-0a1e-0410-9d02-1ff476707afe>
2007-08-21 20:21:19 +0000

    Making the term scraper more modular.

---
+++ trunk/javara/src/com/yellowbkpk/termscrape/MySQLScraperOutput.java
@@ -0,0 +1,67 @@
+package com.yellowbkpk.termscrape;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MySQLScraperOutput implements ScraperOutput {
+
+    private Connection con;
+
+    /**
+     * @param dbHost
+     * @param dbName
+     * @param dbUsername
+     * @param dbPassword
+     */
+    public MySQLScraperOutput(String dbHost, String dbName, String dbUsername, String dbPassword) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://"+dbHost+"/"+dbName, dbUsername, dbPassword);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addSection(Section theSection) {
+        try {
+            Statement statement = con.createStatement();
+            statement.executeUpdate(
+                    "INSERT INTO `classes` " +
+                    "(department, block, section, sectionURL, credits, division, professor, title, freshmanOnly, prerequisite, year, enrollment, capacity, building, roomnumber) "+
+                    "VALUES " +
+                    "('"+theSection.getDepartment()+"'," +
+                    " '"+theSection.getBlock()+"'," +
+                    " '"+theSection.getSectionID()+"'," +
+                    " '"+theSection.getCatalogURL()+"'," +
+                    " '"+theSection.getCredits()+"'," +
+                    " '"+theSection.getDivision()+"'," +
+                    " '"+theSection.getFaculty()+"'," +
+                    " '"+theSection.getTitle()+"'," +
+                    " '"+(theSection.getFreshmanOnly()?1:0)+"'," +
+                    " '"+theSection.getPrerequisites()+"'," +
+                    " '"+theSection.getYear()+"'," +
+                    " '"+theSection.getEnrollment()+"'," +
+                    " '"+theSection.getCapacity()+"'," +
+                    " '"+theSection.getBuilding()+"'," +
+                    " '"+theSection.getRoomnumber()+"')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void cleanup() {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ trunk/javara/src/com/yellowbkpk/termscrape/ScraperOutput.java
@@ -0,0 +1,13 @@
+package com.yellowbkpk.termscrape;
+
+/**
+ * @author Ian Dees
+ *
+ */
+public interface ScraperOutput {
+
+    public void addSection(Section theSection);
+
+    public void cleanup();
+
+}
