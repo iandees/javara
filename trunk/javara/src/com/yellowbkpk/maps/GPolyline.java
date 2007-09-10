5e45a7fb66d9cb062ea3730d988a92ef965113ad
ian.dees <ian.dees@99d860a3-0a1e-0410-9d02-1ff476707afe>
2007-09-10 18:40:49 +0000

    Multiple colors for a polyline.

--- trunk/javara/src/com/yellowbkpk/maps/GPolyline.java
+++ trunk/javara/src/com/yellowbkpk/maps/GPolyline.java
@@ -3,10 +3,10 @@ package com.yellowbkpk.maps;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Stroke;
 import java.awt.image.ImageObserver;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.yellowbkpk.maps.gui.GoogleMapUtilities;
@@ -19,13 +19,13 @@ import com.yellowbkpk.maps.map.GLatLng;
 public class GPolyline extends GOverlay {
 
     private List<GLatLng> myPoints;
-    private Color myColor;
     private GLatLngBounds myBounds;
     private Stroke myStroke;
+    private List<Color> myColors;
 
     public GPolyline(List<GLatLng> points, Color color, int width) {
         myPoints = points;
-        myColor = color;
+        myColors = new ArrayList<Color>();
         myStroke = new BasicStroke(width);
     }
 
@@ -33,21 +33,24 @@ public class GPolyline extends GOverlay {
         Graphics2D g2d = (Graphics2D) dbg2;
         
         Stroke prevStroke = g2d.getStroke();
-        g2d.setColor(myColor);
         g2d.setStroke(myStroke);
         
         int prevX = 0;
         int prevY = 0;
         boolean prevWasIn = false;
         
-        for (GLatLng point : myPoints) {
+        for (int i = 0; i < myPoints.size(); i++) {
+            GLatLng point = myPoints.get(i);
+            Color color = myColors.get(i);
+            
             int x = GoogleMapUtilities.lngToX(point.getLongitude(), 17 - zoom) - bounds.x;
             int y = GoogleMapUtilities.latToY(point.getLatitude(), 17 - zoom) - bounds.y;
             boolean wasIn = false;
             if(viewBounds.contains(point)) {
                 wasIn = true;
             }
-            
+
+            g2d.setColor(color);
             if (prevX != 0 && prevY != 0 && (wasIn || prevWasIn)) {
                 g2d.drawLine(x, y, prevX, prevY);
             }
@@ -81,7 +84,16 @@ public class GPolyline extends GOverlay {
      * @param latLng
      */
     public synchronized void addPoint(GLatLng latLng) {
+        addPoint(latLng, Color.red);
+    }
+
+    /**
+     * @param latLng
+     * @param color
+     */
+    public synchronized void addPoint(GLatLng latLng, Color color) {
         myPoints.add(latLng);
+        myColors.add(color);
         notifyOverlayListeners();
     }
 
